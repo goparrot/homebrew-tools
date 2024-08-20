12aee1b945eee35fbc3f355eec3be0131a2d8b5b
Evghenii Paliiuc <evghenii.p@goparrot.ai>
2024-08-20 14:13:47 +0300

    feat(version-update): set new version goparrot-compose 1.6.0

--- goparrot-compose@1.5.1.rb
+++ goparrot-compose@1.5.1.rb
@@ -1,12 +1,12 @@
 class GoparrotCompose < Formula
     desc "Docker compose generator"
     homepage "https://github.com/goparrot/goparrot-compose"
-    version "v1.6.0"
+    version "v1.5.1"
   
     arch = Hardware::CPU.intel? ? "amd64" : "arm64"
   
     if OS.mac?
-      url "https://goparrot-release-utils.s3.eu-west-1.amazonaws.com/goparrot-compose/goparrot-compose_v1.6.0_darwin_#{arch}.zip"
+      url "https://goparrot-release-utils.s3.eu-west-1.amazonaws.com/goparrot-compose/goparrot-compose_v1.5.1_darwin_#{arch}.zip"
     end
   
     def install
